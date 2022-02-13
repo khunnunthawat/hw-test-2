bcecfb46dad9f08b07e3a1d12df0403111e3587a
nunthawat@daytech.co.th <nunthawat@daytech.co.th>
2022-02-14 01:21:43 +0700

    test-1-value-duplicate

--- app/hw_1.ts
+++ app/hw_1.ts
@@ -2,12 +2,26 @@ import { Transaction } from './interfaces';
 import jsonSampleTransactions from './data/sample_transactions.json';
 import jsonRealTransactions from './data/real_transactions.json';
 
-const sampleTransactions: Transaction[] = jsonSampleTransactions as Transaction[];
+const sampleTransactions: Transaction[] =
+  jsonSampleTransactions as Transaction[];
 const realTransactions: Transaction[] = jsonRealTransactions as Transaction[];
 
 console.log('HW_1 - Application start!');
 console.time('Process time');
 
 // Your code here
+const findArray = sampleTransactions.length;
+console.log('findArray', findArray);
+
+var valueArr = sampleTransactions.map(function (item) {
+  return item.name;
+});
+var isDuplicate = valueArr.some(function (item, idx) {
+  return valueArr.indexOf(item) != idx;
+});
+
+console.log('isDuplicate', isDuplicate);
+
+console.log('realTransactions', realTransactions);
 
 console.timeEnd('Process time');
