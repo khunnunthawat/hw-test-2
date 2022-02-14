13143951b967feaa1db8a17ae921b3532a0f47ce
nunthawat@daytech.co.th <nunthawat@daytech.co.th>
2022-02-14 14:53:30 +0700

    hw-1-done

--- app/hw_1.ts
+++ app/hw_1.ts
@@ -10,18 +10,8 @@ console.log('HW_1 - Application start!');
 console.time('Process time');
 
 // Your code here
-const findArray = sampleTransactions.length;
-console.log('findArray', findArray);
+var valueTest = new Set([...sampleTransactions.map((item) => item.name)]);
 
-var valueArr = sampleTransactions.map(function (item) {
-  return item.name;
-});
-var isDuplicate = valueArr.some(function (item, idx) {
-  return valueArr.indexOf(item) != idx;
-});
-
-console.log('isDuplicate', isDuplicate);
-
-console.log('realTransactions', realTransactions);
+console.log('valueTest', valueTest);
 
 console.timeEnd('Process time');
